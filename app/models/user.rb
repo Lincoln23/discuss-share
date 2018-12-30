a8fa99830a793627e9f4ad5e6aab79326f6ab39d
Lincoln <Lolincoln23@gmail.com>
2018-12-30 17:58:00 -0500

    Finished chap 6

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # before_save {self.email = email.downcase} # Don't need self keyword on the RHS
+  before_save {email.downcase!}
+  validates :name, presence: true, length: {maximum: 50}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 255}, format: VALID_EMAIL_REGEX, uniqueness: {case_sensitive: false}
+
+  has_secure_password
+  validates :password, presence: true,length: {minimum: 6} #:password comes from has_secure_password, it is a virtual attribute
+end
