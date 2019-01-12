64e39b8d82cb03df0126902d8ee2842e369a203b
Lincoln <Lolincoln23@gmail.com>
2019-01-11 21:26:44 -0500

    Add follows feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
@@ -23,6 +27,15 @@ class User < ApplicationRecord
     SecureRandom.urlsafe_base64
   end
 
+  def follow(other_user)
+    following << other_user #append to the end of an array
+  end
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+  def following?(other_user)
+    following.include?(other_user)
+  end
   def remember
     self.remember_token = User.new_token
     # self.remember_digest = User.digest(remember_token) doesn't work b/c of validation
@@ -58,6 +71,7 @@ class User < ApplicationRecord
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
   end
+
   def password_reset_expired?
     reset_sent_at < 2.hours.ago #read as earlier than, password sent earlier than 2 hours ago  12pm sent at < 17-2
   end
